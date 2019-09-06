71c0c042d74984a832c057056fa96e13a38c0351
Radmila <milaejared@outlook.com>
2019-09-06 09:39:28 -0300

    new projects

---
+++ 2 AD - HOC/2342 OverrFlow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2342_OverrFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            int a = int.Parse(s[0]);
+            string x = s[1];
+            int b = int.Parse(s[2]);
+            switch (x)
+            {
+                case "+":
+                    if (a + b <= n) Console.WriteLine("OK");
+                    else Console.WriteLine("OVERFLOW");
+                    break;
+                case "*":
+                    if (a * b <= n) Console.WriteLine("OK");
+                    else Console.WriteLine("OVERFLOW");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2 AD - HOC/2456 Cartas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2456_Cartas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            int c = int.Parse(s[2]);
+            int d = int.Parse(s[3]);
+            int e = int.Parse(s[4]);
+
+            if (a < b && b < c && c < d && d < e) Console.WriteLine("C");
+            else if (a > b && b > c && c > d && d > e) Console.WriteLine("D");
+            else Console.WriteLine("N");
+            Console.ReadKey();
+        }
+    }
+}
