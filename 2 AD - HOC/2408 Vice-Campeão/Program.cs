d1f7f83812ad11158eb27b38f67efc85ea7228db
Radmila <milaejared@outlook.com>
2019-09-06 09:57:39 -0300

    mudanças no 2408, simplificação

---
+++ 2 AD - HOC/2408 Vice-Campeão/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2408_Vice_Campeão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Array.Sort(s);
+            Console.WriteLine(s[1]);
+            Console.ReadKey();
+        }
+    }
+}
