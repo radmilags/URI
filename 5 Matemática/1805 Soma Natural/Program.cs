22066459a475a89af83e1dc227f350cf9bcbe296
Radmila <milaejared@outlook.com>
2019-09-06 09:33:28 -0300

    nova solução e novo problema

---
+++ 5 Matemática/1805 Soma Natural/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1805_Soma_Natural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            long n = b - a + 1;
+            long x = ((a + b) * n) / 2;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
