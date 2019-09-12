44c3c52c072ec6d351b76a86ca198dd2d2cd2e1e
Radmila <milaejared@outlook.com>
2019-09-12 17:11:52 -0300

    continuar

---
+++ 2 AD - HOC/1089 Loop Musical/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1089_Loop_Musical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            while(n != 0)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int[] z = new int[n];
+                for (int i = 0; i < s.Length; i++) z[i] = int.Parse(s[i]);
+
+            }
+        }
+    }
+}
