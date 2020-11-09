3acff25644da6e267354bd89489484f085e25df0
Radmila Gama <milaejared@outlook.com>
2020-11-09 12:51:11 -0300

    Consegui resolver em C, mas em C# está dando erro.

---
+++ 1 Iniciante/1178 Preenchimento de Vetor III/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1178_Preenchimento_de_Vetor_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"N[{i}] = {a:0.0000}");
+                a /= 2;
+            }
+        }
+    }
+}
