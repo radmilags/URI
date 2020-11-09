d769b1e5cb0c845f3b6ac28aaf36982ac7780d36
Radmila Gama <milaejared@outlook.com>
2020-11-09 16:29:17 -0300

    resolvido em c e em c++

--- 1 Iniciante/1178 Preenchimento de Vetor III/Program.cs
+++ 1 Iniciante/1178 Preenchimento de Vetor III/Program.cs
@@ -9,6 +9,7 @@ namespace _1178_Preenchimento_de_Vetor_III
             double a = double.Parse(Console.ReadLine());
             for (int i = 0; i < 100; i++)
             {
+                
                 Console.WriteLine($"N[{i}] = {a:0.0000}");
                 a /= 2;
             }
