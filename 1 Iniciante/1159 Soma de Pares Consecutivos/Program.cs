5059f479c8ea29099ac4bd49659051234c2359a5
Radmila Gama <milaejared@outlook.com>
2020-11-09 16:45:20 -0300

    Feito! :)

---
+++ 1 Iniciante/1159 Soma de Pares Consecutivos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1159_Soma_de_Pares_Consecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            int n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                int x = n;
+                if (x % 2 != 0) x++; soma += x;
+                for (int i = 1; i <= 4; i++)
+                {
+                    x += 2;
+                    soma += x;
+                }
+                Console.WriteLine(soma);
+                soma = 0; x = 0; n = 0;
+                n = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
