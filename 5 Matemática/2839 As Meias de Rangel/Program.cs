268d4484a7102e9d2a78956b7cddfefe7bb21307
Radmila Gama <milaejared@outlook.com>
2020-11-19 10:28:09 -0300

    Feito.

---
+++ 5 Matemática/2839 As Meias de Rangel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2839_As_Meias_de_Rangel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            x++; Console.WriteLine(x);
+        }
+    }
+}
