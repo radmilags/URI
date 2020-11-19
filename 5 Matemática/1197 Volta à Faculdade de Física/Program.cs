4a87aa7ec790df0b820699ae42b28c2dff7904aa
Radmila Gama <milaejared@outlook.com>
2020-11-19 09:57:51 -0300

    Feito.

---
+++ 5 Matemática/1197 Volta à Faculdade de Física/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1197_Volta_à_Faculdade_de_Física
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            while(!string.IsNullOrEmpty(s = Console.ReadLine()))
+            {
+                string[] s1 = s.Split(' ');
+                int v = int.Parse(s1[0]);
+                int t = int.Parse(s1[1]);
+                Console.WriteLine($"{v*t*2}");
+            }
+        }
+    }
+}
